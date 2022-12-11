963c6e705417672d9c2ef4b952c4b63748416650
Govardhan5612 <govardhansiripireddy5612@gmail.com>
2022-12-11 23:43:55 +0530

    Ability to create binary search tree

---
+++ com/bridgelabz/BinaryTree/Node.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.BinaryTree;
+
+/**
+ * @author Govardhan Reddy
+ */
+public class Node {
+    /**
+     * In Binary search tree each node divided into two nodes
+     * one is left node
+     * two is right node
+     */
+    int data;
+    Node left;
+    Node right;
+
+    public Node(int data) {
+        this.data = data;
+    }
+}
